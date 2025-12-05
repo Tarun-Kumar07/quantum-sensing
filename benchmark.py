10ade23a444004120b3d20fd253096b53a504a1a
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2025-12-05 16:47:05 -0500

    Trivial bug

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 import timeit
 
@@ -19,15 +21,15 @@ def generate_circuit_parameters(num_qubits, num_blocks):
     }
 
 def benchmark(name):
-    qubits = range(4, 13)
-    blocks = range(2, 5)
+    qubits = list(range(4, 13))
+    blocks = list(range(2, 5))
     filename = f"./benchmark.txt"
 
-    for qubits, blocks in [(q, b) for q in qubits for b in blocks]:
-        circuit_params = generate_circuit_parameters(qubits, blocks)
-        execution_time = timeit.timeit(lambda: run_circuit(circuit_params, hamiltonian_parameters), number=10)
-        with open(filename, "w") as f:
-            f.write(f"{name} - Qubits: {qubits}, Blocks: {blocks}, Average Execution time 10 runs: {execution_time/10} seconds \n")
+    with open(filename, "w") as file:
+        for qubits, blocks in list(product(qubits, blocks)):
+            circuit_params = generate_circuit_parameters(qubits, blocks)
+            execution_time = timeit.timeit(lambda: run_circuit(circuit_params, hamiltonian_parameters), number=10)
+            file.write(f"{name} - Qubits: {qubits}, Blocks: {blocks}, Average Execution time 10 runs: {execution_time/10} seconds \n")
 
 if __name__ == '__main__':
     benchmark("quspin")
