5b4f6cefad537db32f7f33a8e65372cf65bfcf83
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2025-12-28 13:20:17 -0500

    Remove conda initialization

--- tests/run_benchmarks.sh
+++ tests/run_benchmarks.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-conda activate quantum-sensing
 for i in {1..15}; do
   echo "Running test with $i threads..."
   pytest perf_test.py -s --junitxml="../data/perf_results_test_${i}_threads.xml" --num-threads="$i"
